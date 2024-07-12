bd018c2aacf2a8bd28ea5c142458df47a8dcabdd
Raul Avila Catala <cl2raul66@hotmail.com>
2024-07-12 11:26:32 -0600

    Ajustes básicos

---
+++ ReciboGeneratorApp/ReciboGeneratorApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ReciboGeneratorApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
